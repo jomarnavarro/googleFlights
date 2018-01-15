e2635f81557af430574e6ba098e8423672f02001
onavarroKueski <onavarro@kueski.com>
2018-01-14 20:30:02 -0600

    Removes references to old project

--- features/step_definitions/all_in_media_steps.rb
+++
@@ -1,9 +0,0 @@
-Then(/^User will be on In all Media page with "([^"]*)"$/) do |title|
-  ensure_page(InAllMediaPage)
-  @page.validate(title)
-end
-
-Then(/^The In All Media Logo will be displayed$/) do
-  ensure_page(InAllMediaPage)
-  @page.validate_logo
-end
--- features/step_definitions/google_search_steps.rb
+++
@@ -1,9 +0,0 @@
-Given(/^User google searches "([^"]*)"$/) do |keyword|
-  ensure_page(GoogleSearchPage)
-  @page.search(keyword)
-end
-
-When(/^User clicks on link leading to in all media url$/) do
-  ensure_page(GoogleSearchPage)
-  @page.click_in_all_media_url
-end
